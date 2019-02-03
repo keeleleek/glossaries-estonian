f5be107b95a329af7c8321ca0f58e4ed9f87c579
kristiank <kristian@keeleleek.ee>
2019-02-03 11:01:43 +0200

    helper script to create zip archive for CTAN

---
+++ make-ctan-package.sh
@@ -0,0 +1,6 @@
+cd ..
+zip -r glossaries-estonian.zip \
+    glossaries-estonian/glossaries-estonian.dtx \
+    glossaries-estonian/glossaries-estonian.ins \
+    glossaries-estonian/glossaries-estonian.pdf \
+    glossaries-estonian/README
